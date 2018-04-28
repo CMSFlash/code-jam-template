7bd2288451cad4e3ad1dd0bcbe218f3fedd24658
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-04-28 20:52:14 +0800

    Improve the template test case generator.

--- gen.py
+++ gen.py
@@ -10,8 +10,9 @@ def legalize(n):
 
 
 
-L = int(1e6)
 T = 1
+N = 1000
+L = int(1e6)
 
 
 
@@ -20,6 +21,18 @@ print(T)
 
 for case_id in range(T):
 
-    l = legalize(random.randint(1, L))
-    f = legalize(random.randint(1, l))
-    print(f, l)
+    n = random.randint(1, N)
+
+    ls = []
+    fs = []
+
+    for i in range(n):
+
+        l = legalize(random.randint(1, L))
+        f = legalize(random.randint(1, l))
+
+    print(n)
+    
+    for l, f in zip(ls, fs):
+
+        print(l, f)
