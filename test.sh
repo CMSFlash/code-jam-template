8e88addf25b0baf56aed0dded8686fab271c3c14
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-04-28 21:04:33 +0800

    Move to use pypy3 as the default executor.

--- test.sh
+++ test.sh
@@ -2,18 +2,18 @@ i=0
 while :
 do
     echo "Testing case $i"
-    python3 gen.py > test.txt
+    pypy3 gen.py > test.txt
 
-    python3 $1 < test.txt > a.txt
+    pypy3 $1 < test.txt > a.txt
     if (($? != 0))
     then
         break
     fi
-    if [ $2 == "smoke" ]
+    if [ $2 == "organic" ]
     then
         cp a.txt b.txt
     else
-        python3 $2 < test.txt > b.txt
+        pypy3 $2 < test.txt > b.txt
     fi
 
     difference=`diff a.txt b.txt`
