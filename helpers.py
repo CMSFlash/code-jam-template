b41f56222a6baca9ad2896f331e91abb591be125
Shen Zhuo-Ran <cmsflash99@gmail.com>
2018-05-06 16:22:05 +0800

    Merge module helpers to sol, since Code Jam 2018 requires single-file submissions.

--- helpers.py
+++
@@ -1,8 +0,0 @@
-class PrintForDebug:
-
-    def __init__(self, debug):
-        self.debug = debug   
-
-    def __call__(self, *args, **kwargs):
-        if self.debug:
-            print(*args, **kwargs)
--- sol.py
+++ sol.py
@@ -1,6 +1,20 @@
-from sys import stdin
+# Helpers
 
-from helpers import *
+
+class PrintForDebug:
+
+    def __init__(self, debug):
+        self.debug = debug
+
+    def __call__(self, *args, **kwargs):
+        if self.debug:
+            print(*args, **kwargs)
+
+
+# Solution
+
+
+from sys import stdin
 
 
 print_for_debug = PrintForDebug(False)
